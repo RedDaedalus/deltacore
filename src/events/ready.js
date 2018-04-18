12063e3a68fb75a652b04f7498941cbc6a84406d
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-04-17 21:24:43 -0600

    fixed game

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 function ready(client) {
     client.log("login", "Login finished.", "green");
 
-    client.user.setActivity(`for _help | ${client.guilds.size} servers`, { type: "watcihng" });
+    client.user.setActivity(`for _help | ${client.guilds.size} servers`, { type: "WATCHING" });
 }
 
 module.exports = ready;
