2efe49fb48b1f23d7c70a2f1fe4a07199d4f86e6
RedstoneDaedalus <redstonedaedalus@gmail.com>
2018-04-18 17:31:40 -0600

    raw command

---
+++ src/commands/raw.js
@@ -0,0 +1,30 @@
+const Command = require("../classes/Command");
+
+class Raw extends Command{
+    constructor(client) {
+        super(client, {
+            help: {
+                name: "raw",
+                description: "Shows server settings in JSON form. Mainly used for debugging.",
+                usage: "[guild]"
+            },
+            conf: {
+                ownerOnly: true
+            }
+        });
+    }
+
+    async run(message, args) {
+        // Fetch the guild
+        const guild = args[0] ? this.client.guilds.get(args[0]) : message.guild;
+        // Make sure that the guild found is available 
+        if (!guild || !guild.available) return message.channel.error("The specified guild either does not exist, or is unavailable.");
+
+        // Fetch the guild's server settings
+        const settings = await guild.fetchSettings();
+        // Send the settings in JSON form   
+        message.channel.respond(`Server settings for \`${guild.name}\`:\n\`\`\`json\n${JSON.stringify(settings, null, 2)}\`\`\``);
+    }
+}
+
+module.exports = Raw;
